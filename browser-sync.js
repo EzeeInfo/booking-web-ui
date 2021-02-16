7f765d0676e02503061364a0ad5312b104024eb0
Haripriya Ravichandran <31511082+rmhari@users.noreply.github.com>
2021-02-16 07:24:34 +0530

    Operator service wired (#1)
    * wired
    
    * operator changes
    
    * operator operations added
    
    * add functionality
    
    * preview button added
    
    * seet layout dynamic rendering done
    
    Co-authored-by: apt-get2Update <thirumurugan3131@gmail.com>

--- browser-sync.js
+++ browser-sync.js
@@ -18,29 +18,15 @@ module.exports = {
      |--------------------------------------------------------------------------
      | https://github.com/shakyShane/browser-sync/wiki/options#wiki-files
      */
-    files: ["dist/css/*.css", "dist/js/*.js","dist/**/*.html"],
-    middleware: [
-        {
-            route: "/api",
-            handle: function (req, res, next) {
-
-                res.setHeader('Content-Type', 'application/json');
-
-                if(req.url == "/login") {
-                    if(req.method == 'POST') {
-                        const username = req.body.username;
-                        const password = req.body.password;
-                        res.statusCode = 200;
-                        res.end('Hello, World!\n');
-                    }else {
-                        res.statusCode = 405;
-                        res.end('Invalid\n');
-                    }
-                }
-                next();
+    files: ["dist/css/*.css", "dist/js/*.js", "dist/**/*.html"],
+    proxy: {
+        target: "http://localhost:8080",
+        proxyRes: [
+            function(proxyRes, req, res) {
+                console.log(proxyRes.headers);
             }
-        }
-    ],
+        ]
+    },
     serveStatic: ['dist']
 
 };
---
+++ src/js/operators.js
@@ -0,0 +1,82 @@
+window.onload = () => {
+    fetchOperators();
+    document.querySelector('button.btn:nth-child(2)').addEventListener('click', saveOperator)
+
+};
+
+var isUpdate = false;
+
+function createColumn(text) {
+    const td = document.createElement('td');
+    td.innerHTML = text;
+    return td;
+}
+
+function fetchOperators() {
+
+    fetch("/api/operators", {
+
+    }).then(res => res.json()).then(operators => {
+        const tBody = document.querySelector('table>tbody');
+        tBody.innerHTML = '';
+        operators.map(operator => {
+            const tr = document.createElement('tr');
+            tr.appendChild(createColumn(operator.code));
+            tr.appendChild(createColumn(operator.name));
+            tr.appendChild(createColumn(`<span class="btn btn-outline-primary" onclick="currentPlace('${operator.code}','${operator.name}')" id="editModal" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            <i class="far fa-edit"></i>
+            </span> 
+            <span  >
+            <a class="btn btn-outline-primary" href="http://localhost:8080/${operator.code}" target="_BLANK">
+                <i class="fa fa-eye" aria-hidden="true"></i>
+                </a>
+            </span> 
+            `))
+            tBody.appendChild(tr);
+        });
+    })
+}
+
+function currentPlace(code, name) {
+    isUpdate = true;
+    document.getElementById("operatorCode").value = code;
+    document.getElementById("operatorName").value = name;
+}
+
+async function saveOperator() {
+    const code = document.getElementById("operatorCode").value;
+    const name = document.getElementById("operatorName").value;
+    const operator = { code, name };
+    if (isUpdate) {
+        update(operator);
+    } else {
+        create(operator);
+    }
+    fetchOperators();
+    document.getElementById("operatorCode").value = ""
+    document.getElementById("operatorName").value = ""
+}
+
+async function create(operator) {
+    const res = await fetch("/api/operators", {
+        method: "POST",
+        body: JSON.stringify(operator),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+    const data = res.json();
+    isUpdate = false;
+}
+
+async function update(operator) {
+    const res = await fetch("/api/operators/" + operator.code, {
+        method: "PUT",
+        body: JSON.stringify(operator),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+    const data = res.json();
+    isUpdate = false;
+}
