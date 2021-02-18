8351614119bbcc951ccc18b8d3c7e5495a547a6c
tajulkhan <tajulkhan@users.noreply.github.com>
2021-02-18 14:03:48 +0000

    Automated deployment: Thu Feb 18 14:03:48 UTC 2021 49197a514bf3016fd6065367b0268b220a937cbd

--- js/index.js
+++ js/index.js
@@ -1,3 +1,26 @@
 "use strict";
 window.onload = function () {
+    document
+        .getElementById("fromStation")
+        .addEventListener("change", selectStation, false);
 };
+function createOption(value, text, isSelected) {
+    if (value === void 0) { value = ""; }
+    if (text === void 0) { text = "--"; }
+    if (isSelected === void 0) { isSelected = false; }
+    var option = document.createElement("option");
+    option.selected = isSelected;
+    option.value = value;
+    option.innerHTML = text;
+    return option;
+}
+function selectStation(e) {
+    var selectedOption = e.target.selectedOptions[0];
+    var options = selectedOption.getAttribute("data-to").split(",");
+    var toStation = document.getElementById("toStation");
+    toStation.innerHTML = "";
+    toStation.append(createOption("", "--", true));
+    options.map(function (option) {
+        return toStation.append(createOption(option, option));
+    });
+}
