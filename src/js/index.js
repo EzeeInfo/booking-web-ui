1b12047dbbf0ec3e9707cc2ccc5a48f6e9b33b81
apt-get2Update <thirumurugan3131@gmail.com>
2021-02-18 19:19:10 +0530

    from to station handling

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,23 @@
 window.onload = () => {
-    
+  document
+    .getElementById("fromStation")
+    .addEventListener("change", selectStation, false);
 };
+
+function createOption(value = "", text = "--", isSelected = false) {
+  const option = document.createElement("option");
+  option.selected = isSelected;
+  option.value = value;
+  option.innerHTML = text;
+  return option;
+}
+function selectStation(e) {
+  let selectedOption = e.target.selectedOptions[0];
+  let options = selectedOption.getAttribute("data-to").split(",");
+  const toStation = document.getElementById("toStation");
+  toStation.innerHTML = "";
+  toStation.append(createOption("", "--", true));
+  options.map((option) => {
+    return toStation.append(createOption(option, option));
+  });
+}
