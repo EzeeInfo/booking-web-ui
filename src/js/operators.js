5ec4324664e14e9121405b6b1aacf127c5600b14
haripriya <rm.haripriya@gmail.com>
2021-02-19 17:14:05 +0530

    sdsss

--- src/js/operators.js
+++ src/js/operators.js
@@ -1,130 +1,118 @@
-var exampleModal;
-var code;
-var confermationModal;
-window.onload = () => {
-    fetchOperators();
-    document.querySelector('button.btn:nth-child(2)').addEventListener('click', saveOperator)
-    exampleModal = new bootstrap.Modal(document.getElementById('exampleModal'), {
-        keyboard: false
-    })
-    confermationModal = new bootstrap.Modal(document.getElementById('deleteModal'), {
-        keyboard: false
-    })
-};
+var selectedRow = null;
 
-var isUpdate = false;
-
-function createColumn(text) {
-    const td = document.createElement('td');
-    td.innerHTML = text;
-    return td;
-}
-
-function fetchOperators() {
-
-    fetch("/api/operators", {
-
-    }).then(res => res.json()).then(operators => {
-        const tBody = document.querySelector('table>tbody');
-        tBody.innerHTML = '';
-        if (operators.length == 0) {
-            tBody.innerHTML = `<tr class="text-center">
-                            <td colspan="3"> <span class="text-muted font-italic"> No Records
-                            Found ... Add your data to <a href="" data-bs-toggle="modal"
-                                data-bs-target="#exampleModal">click here...</a></span></td>
-                        </tr>`
-        }
-
-        operators.map(operator => {
-            const tr = document.createElement('tr');
-            tr.appendChild(createColumn(operator.code));
-            tr.appendChild(createColumn(operator.name));
-            tr.appendChild(createColumn(`<span 
-            class="btn btn-outline-primary" 
-            onclick="currentPlace('${operator.code}','${operator.name}')"
-            id="editModal" data-bs-toggle="modal" 
-            data-bs-target="#exampleModal">
-            <i class="far fa-edit"></i>
-            </span> 
-            <span>
-            <a class="btn btn-outline-primary" href="/${operator.code}" target="_BLANK">
-                <i class="fa fa-eye" aria-hidden="true"></i>
-                </a>
-            </span> 
-            <span 
-            class="btn btn-outline-primary" 
-            data-bs-toggle="modal" 
-            onclick="deleteOperator('${operator.code}')"
-            data-bs-target="#deleteModal" >
-            
-                <i class="fa fa-trash" aria-hidden="true"></i>
-                
-            </span> 
-            `))
-            tBody.appendChild(tr);
-        });
-    })
-}
-
-function currentPlace(code, name) {
-    isUpdate = true;
-    document.getElementById("operatorCode").value = code;
-    document.getElementById("operatorName").value = name;
-}
-
-async function saveOperator() {
-    const code = document.getElementById("operatorCode").value;
-    const name = document.getElementById("operatorName").value;
-    const operator = { code, name };
-    if (isUpdate) {
-        await update(operator);
-    } else {
-        await create(operator);
+class Operator {
+    constructor(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
     }
-    await fetchOperators();
-    document.getElementById("operatorCode").value = ""
-    document.getElementById("operatorName").value = ""
-    exampleModal.hide();
 }
 
-async function create(operator) {
-    const res = await fetch("/api/operators", {
-        method: "POST",
-        body: JSON.stringify(operator),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
+class UI {
+
+    static displayoperator() {
+        const operator = [
+        ];
+        operator.forEach((book) => UI.AddOperatorToList(book));
+    }
+
+    static AddOperatorToList(operator) {
+        const list = document.querySelector("#operator-list");
+        const row = document.createElement("tr");
+        row.innerHTML = `
+        <td>${operator.title}</td>
+        <td>${operator.author}</td>
+        <td>${operator.isbn}</td>
+        <td><a href="#" class="btn btn-success btn-sm edit">Edit</a></td>
+        <td><a href="#" class="btn btn-danger btn-sm delete">Delete</a></td>
+        `
+        list.appendChild(row);
+    }
+
+    static editOperatorToList(operator) {
+        selectedRow.children[0].textContent = operator.title;
+        selectedRow.children[1].textContent = operator.author;
+        selectedRow.children[2].textContent = operator.isbn;
+
+        document.querySelector(".sumbit_btn").value = "Submit";
+        document.querySelector(".sumbit_btn").classList = "btn btn-success btn-block add-btn sumbit_btn";
+    }
+    static deleteEmpyee(el) {
+        if (el.classList.contains("delete")) {
+            el.parentElement.parentElement.remove();
+            UI.showAlert("Operator deleted", "danger");
+        } else {
+            null;
         }
-    })
-    const data = await res.json();
-    isUpdate = false;
-}
+    }
 
-function deleteOperator(operatorCode) {
-    code = operatorCode;
+    // popunjavanje input polja sa podacima iz zeljenog reda
+    static editOperator(el) {
+        if (el.classList.contains('edit')) {
+            selectedRow = el.parentElement.parentElement;
+            document.querySelector("#title").value = selectedRow.children[0].textContent;
+            document.querySelector("#author").value = selectedRow.children[1].textContent;
+            document.querySelector("#isbn").value = selectedRow.children[2].textContent;
+
+            document.querySelector(".sumbit_btn").value = "Update";
+            document.querySelector(".sumbit_btn").classList = "btn btn-primary btn-block add-btn sumbit_btn";
+        } else { null }
+    }
+
+    // message toast
+    static showAlert(message, className) {
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.container');
+        const main = document.querySelector('.main');
+        container.insertBefore(div, main);
+        div.style.position = "absolute";
+        div.style.top = "30px";
+        div.style.left = "90%";
+        setTimeout(() => document.querySelector('.alert').remove(), 3000);
+    }
+
+    // reset fields
+    static clearFields() {
+        document.querySelector('#title').value = '';
+        document.querySelector('#author').value = '';
+        document.querySelector('#isbn').value = '';
+    }
+
+
+
 }
-async function remove() {
-    const res = await fetch("/api/operators/" + code, {
-        method: "DELETE",
-        headers: {
-            "Content-type": "application/json"
+//  end of UI CLASS
+
+document.addEventListener('DOMContentLoaded', UI.displayoperator);
+
+document.querySelector('#operator-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+
+    if (title === '' || author === '' || isbn === '') {
+        UI.showAlert('Please fill in all fields', 'danger');
+    } else {
+        const operator = new Operator(title, author, isbn);
+
+        if (selectedRow == null) {
+            UI.AddOperatorToList(operator);
+            selectedRow = null;
+            UI.showAlert('Operator Added', 'success');
+        } else {
+            UI.editOperatorToList(operator);
+            selectedRow = null;
+            UI.showAlert('Operator Info Edited', 'info');
         }
-    })
-    res.json().then(d => {
-        fetchOperators();
-    });
-    code = null;
-    confermationModal.hide()
+        UI.clearFields();
+    }
+});
 
-}
-
-async function update(operator) {
-    const res = await fetch("/api/operators/" + operator.code, {
-        method: "PUT",
-        body: JSON.stringify(operator),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
-        }
-    })
-    const data = await res.json();
-    isUpdate = false;
-}
+document.querySelector('#operator-list').addEventListener('click', (e) => {
+    UI.deleteEmpyee(e.target);
+    UI.editOperator(e.target);
+});
